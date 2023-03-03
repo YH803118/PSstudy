54e55df02842faff55f3b0691a1711a803fc32f0
YH803118 <dudghks1045@naver.com>
2023-03-03 17:16:40 +0900

    0303

--- 20230302.js
+++ 20230302.js
@@ -1,28 +1,38 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/72412
 // 순위 검색
 // 정확성 클리어 / 효율성 x
+function binarySearch(arr, target) {
+  var start = 0;
+  var end = arr.length - 1;
+  var mid;
+  while (start <= end) {
+    mid = parseInt((start + end) / 2);
+    if (Number(target) > arr[mid][4]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return end;
+}
+
 function solution(info, query) {
   var answer = [];
+
   var infoSpl = info.map((x) => x.split(" "));
   infoSpl = infoSpl.sort((a, b) => {
     return b[4] - a[4];
   });
   var test = infoSpl.map((x) => x[4]);
   var querySpl = query.map((x) => x.split(/ and | /));
-
   for (var q of querySpl) {
     var ans = 0;
-    for (var i of infoSpl) {
-      var score = 0;
-      if (Number(q[4]) > i[4]) break;
+    var search = binarySearch(infoSpl, q[4]);
+    for (let i = 0; i <= search; i++) {
       for (var j = 0; j <= 3; j++) {
-        if (q[j] === "-") {
-          score++;
-        } else if (q[j] !== i[j]) {
-          break;
-        } else score++;
+        if (q[j] !== "-" && q[j] !== infoSpl[i][j]) break;
+        else if (j === 3) ans++;
       }
-      if (score === 4) ans++;
     }
     answer.push(ans);
   }
